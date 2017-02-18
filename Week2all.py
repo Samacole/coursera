a80558dc4bb70bb9f884b878752e2961905dc678
Samacole <cole.samuel2@gmail.com>
2017-02-18 11:46:16 +0000

    added in aprox pattern match and count, both implimented and tested

--- Week2all.py
+++ Week2all.py
@@ -19,8 +19,8 @@ def Minskew(Genome):
     minindex = [i for i in range(0,len(skewlist)) if skewlist[i] == minvalue]
     return minindex
 
-# code to find the 'Hammingdistance' between 2 patterns of equal lengh (the differnce in bases)
-def HammingDistance(p, q):
+# code to find the 'Hammingdistance' between 2 patterns of equal lengh (the int differnce in bases)
+def hammingdistance(p, q):
     count = 0
     Ham = 0
     for i in range(0, len(p)):
@@ -33,3 +33,27 @@ def HammingDistance(p, q):
 
 # code to output the positions of pattern within the genome where pattern approx matches, max hammingdistance d
 def aproxpatternmatch (pattern, text, d): # input - pattern being looked for, genome and max hamming distance
+    indlist = []
+    for i in range(len(text)-len(pattern)+1):
+        check = text[i:i+len(pattern)]
+        checked = hammingdistance(pattern, check)
+        if checked <= d:
+            indlist.append(i)
+    return (indlist)
+
+#code to impliment appox pattern count
+#input - pattern, text and d (approve mismatches aloud as int)
+#output - count of pattern within text given the number of missmatches aloud
+def aproxpatterncount(pattern, text, d):
+    count = 0
+    for i in range(0, len(text) - len(pattern) + 1):
+        pattern2 = text[i:i + len(pattern)]
+        if hammingdistance(pattern, pattern2) <= d:
+            count = count + 1
+    return (count)
+
+pat = "GCTCCA"
+gen = "GGGTGCGGGTGGAATATTCGGGGACAATCCATTCTCGTTTGAACTACTCCAGGATTCGAGTTGGCTCCACGTATTATGTCGCAGTAATGCCTTAGGAAAAATCATACCCTCTTGAGCGTGACCGCTCTAATCCTTTAAAATGAATCGCATACAGGCAACCCACTTCGAAGAAAACACTAAGTTCTGAAAAGAGCTACAGTCTCCTGTGTGTATAAAGAGCCCACTACATTGTGCGCTACGCGTCCTCCCCCTTCTTTGTGCGAGTGGATCGGGTTCTAGCGGACAATACCAAGCATTAGAATGGGCTATGTAAGATCCACCCAATTTACCATATTAACTAGACTAACGCTGCAAGGACTGGCAAGGAGGTGCTAAGGACAAGTTAT"
+d = 3
+
+print(aproxpatterncount(pat, gen, d))
